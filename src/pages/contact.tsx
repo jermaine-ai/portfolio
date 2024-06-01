46997f6225daac4a6173009fc5f2e1d658ffafa3
Jay Bedeau-Stark <jay@unflawed.dev>
2024-06-01 14:01:20 +0100

    added a contact form

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   //   BackgroundAnimation,
   //   ButtonGroup,
@@ -8,10 +9,70 @@ import {
   //   SubHeader,
 } from "../components";
 
+export const AnimatedTimeline = () => <pre>Animated Timeline</pre>;
+export const SkillBadges = () => <pre>Skills</pre>;
+export const Testimonials = () => <pre>Testimonials</pre>;
+export const ContactForm = () => {
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        alert(JSON.stringify(formValues, null, 2));
+      }}
+    >
+      <fieldset>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            onChange={(e) =>
+              setFormValues({ ...formValues, name: e.target.value })
+            }
+            type="text"
+            name="name"
+            id="name"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={(e) =>
+              setFormValues({ ...formValues, email: e.target.value })
+            }
+            type="email"
+            name="email"
+            id="email"
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            onChange={(e) =>
+              setFormValues({ ...formValues, message: e.target.value })
+            }
+            name="message"
+            id="message"
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </fieldset>
+    </form>
+  );
+};
+
 export function ContactPage() {
   return (
     <Page>
       <pre>Contact Page</pre>
+      <AnimatedTimeline />
+      <Testimonials />
+      <ContactForm />
     </Page>
   );
 }
