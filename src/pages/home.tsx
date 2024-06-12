0d66120f6f92d4effe453028609b0f7ab08ab2ce
Jay Bedeau-Stark <jay@unflawed.dev>
2024-06-12 14:52:50 +0100

    (feat): Simplified styles for homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,9 @@
+import styled from "styled-components";
 import {
   BackgroundAnimation,
   ButtonGroup,
-  Carousel,
   Header,
   Page,
-  Section,
   SubHeader,
 } from "../components";
 
@@ -16,61 +15,70 @@ export const Client = () => {
   );
 };
 
+const ClientSection = styled.section`
+  margin-top: 120px;
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+`;
+const HomeContainer = styled.div`
+  background: cyan;
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding-top: 10%;
+  overflow: hidden;
+`;
+const NavStyles = styled.nav`
+  padding: 0 24px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  // background: #333;
+  height: 40px;
+  width: 100%;
+`;
+const HomeStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 640px;
+  width: 100%;
+`;
+
 export function HomePage() {
   return (
     <Page
       sections={{
-        // nav: (
-        //   <div style={{ background: "#333", height: "40px", width: "100%" }}>
-        //     <pre style={{ color: "white" }}>Nav</pre>
-        //   </div>
-        // ),
+        nav: <></>,
         fluid: (
-          <div
-            style={{
-              background: "cyan",
-              height: "100vh",
-              width: "100%",
-              display: "flex",
-              justifyContent: "center",
-              paddingTop: "10%",
-              overflow: "hidden",
-            }}
-          >
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                maxWidth: "640px",
-                width: "100%",
-              }}
-            >
-              <BackgroundAnimation animation={"fluid"} />
-              <span>Hi, I'm Jay</span>
-              <Header text={"Web, Mobile & Blockchain Developer"} />
-              <SubHeader text={"Building the future, one block at a time."} />
-              <ButtonGroup
-                buttons={[
-                  { link: "/work", text: "View My Work" },
-                  { link: "/about", text: "About Me" },
-                  { link: "/contact", text: "Get in Touch" },
-                ]}
-              />
-              <div
-                style={{
-                  marginTop: "120px",
-                  display: "flex",
-                  justifyContent: "space-evenly",
-                  width: "100%",
-                }}
-              >
-                <Client />
-                <Client />
-                <Client />
-                <Client />
-              </div>
-            </div>
-          </div>
+          <>
+            <HomeContainer>
+              <NavStyles>
+                <pre style={{ color: "white" }}>Nav</pre>
+              </NavStyles>
+              <HomeStyles>
+                <BackgroundAnimation animation={"fluid"} />
+                <span>Hi, I'm Jay</span>
+                <Header text={"Web, Mobile & Blockchain Developer"} />
+                <SubHeader text={"Building the future, one block at a time."} />
+                <ButtonGroup
+                  buttons={[
+                    { link: "/work", text: "View My Work" },
+                    { link: "/about", text: "About Me" },
+                    { link: "/contact", text: "Get in Touch" },
+                  ]}
+                />
+                <ClientSection>
+                  <Client />
+                  <Client />
+                  <Client />
+                  <Client />
+                </ClientSection>
+              </HomeStyles>
+            </HomeContainer>
+          </>
         ),
       }}
     >
